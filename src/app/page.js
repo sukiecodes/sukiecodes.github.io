ef10012de54c61dd7cd9a7500418f76220415192
Sukie Zhang <sukiezhang.sz@gmail.com>
2025-01-13 07:29:33 -0500

    updated README.md and fixed typing speed for homepage

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
             "software engineer",
             "creative",
             "student",
-            "foodie & chef",
+            "foodie/chef",
             "full-stack developer",
           ]}
           typeSpeed={100} 
