a18de0f6c2c0d74eab1250ae2b101f8ea9aab6ec
Sukie Zhang <sukiezhang.sz@gmail.com>
2026-09-19 19:19:34 -0400

    added image to right of page

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import Layout from '@/components/Layout';
 
 export default function Home() {
@@ -20,7 +21,7 @@ export default function Home() {
             intention
           </h1>
           <p className="text-[16px] text-[#7a5a48] leading-[1.75] my-6 max-w-[440px] font-light">
-            I’m a Boston University alum with a degree in Computer Science, based in NYC. Passionate about thoughtful, human-centered technology that expands access and creates meaningful impact.
+            I’m a recent graduate of Boston University, currently based in NYC. Passionate about thoughtful, human-centered technology that expands access and creates meaningful impact.
             Outside of coding, you can find me collecting stickers for my scrapbook 𓄲, eating copious amounts of frozen yogurt (๑ᵔ⤙ᵔ๑), wandering to new restaurants 𓐐, browsing GoodReads for my next read ꩜, or making new syrups for my daily lattes ִ ࣪𖤐.
           </p>
           
@@ -40,6 +41,39 @@ export default function Home() {
               <span className="w-2 h-2 rounded-full bg-[#2a1a12]"></span>GitHub
             </a>
           </div>
+        </div>
+
+        {/* Right: taped-in photo */}
+        <div className="relative flex justify-center lg:justify-end">
+          {/* soft pink wash behind the photo */}
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute -top-10 right-4 w-72 h-72 rounded-full bg-[#fce4eb] blur-3xl opacity-70"
+          ></div>
+
+          <figure className="group relative -rotate-[2.5deg] bg-white rounded-[20px] p-3 pb-16 border border-[#c89678]/20 shadow-[0_24px_50px_-24px_rgba(42,26,18,0.4)] transition-transform duration-500 ease-out hover:rotate-0">
+            {/* washi tape */}
+            <span
+              aria-hidden="true"
+              className="absolute -top-3 left-10 w-24 h-7 -rotate-6 bg-[#fce4eb]/80 border-x border-[#e85d7a]/20"
+            ></span>
+            <span
+              aria-hidden="true"
+              className="absolute -top-4 right-12 w-20 h-7 rotate-6 bg-[#f4e2d2]/85 border-x border-[#c89678]/25"
+            ></span>
+
+            <div className="relative w-[270px] sm:w-[320px] lg:w-[360px] aspect-[4/5] overflow-hidden rounded-[12px] bg-[#f6ece4]">
+              <Image
+                src="/images/sukie.jpg"
+                alt="Sukie Zhang"
+                fill
+                priority
+                sizes="(max-width: 640px) 270px, (max-width: 1024px) 320px, 360px"
+                className="object-cover"
+              />
+            </div>
+
+          </figure>
         </div>
       </section>
     </Layout>
